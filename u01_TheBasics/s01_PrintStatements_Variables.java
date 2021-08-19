ae70d22cce853f1903a2e2e36c2f8ec8eec542a0
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-08-19 07:20:01 -0500

    Print Statements and Variables

---
+++ u01_TheBasics/s01_PrintStatements_Variables.java
@@ -0,0 +1,26 @@
+public class s01_PrintStatements_Variables {
+
+    public static void main(String[] args) {
+
+        //Print Statements
+        System.out.println("Hello world!");
+        System.out.println("This is a sentence");
+
+        System.out.print("Hello world!");
+        System.out.print("This is a sentence");
+
+        //Variables
+        int number = 9;
+        double number2 = 9.1;
+        String word = "This is a sentence stored in a variable.";
+        boolean isItRaining = true;
+        boolean isItCloudy = false;
+
+        //Printing out something stored in a variable
+        System.out.println(word);
+        System.out.println(number2);
+        System.out.println(isItRaining);
+
+
+    }
+}
