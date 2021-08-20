f269e6d4b6f40e0e46180948833787f6c1c1c99d
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-08-20 13:02:48 -0500

    Basic Operators

---
+++ u01_TheBasics/s02_Operators.java
@@ -0,0 +1,11 @@
+public class s02_Operators {
+    public static void main(String[] args) {
+
+        int addition = 8+2;
+        int subtraction = 9-2;
+        int multiplication = 3*5;
+        int division = 8/2;
+
+    }
+
+}
