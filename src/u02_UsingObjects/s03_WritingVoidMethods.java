091c4c02372f4313a4d2c8478047b6c631acc579
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-09-03 08:39:36 -0500

    AnimationTemplate

--- src/u02_UsingObjects/s03_WritingVoidMethods.java
+++ src/u02_UsingObjects/s03_WritingVoidMethods.java
@@ -2,16 +2,16 @@ package u02_UsingObjects;
 
 public class s03_WritingVoidMethods {
     public static void main(String[] args){
-        drawTriangle(0.2, 0.3);
-        drawTriangle(.8, .6);
+        drawTriangle(0.2, 0.3, 1.2);
+        drawTriangle(.8, .6, 0.7);
 
     }
 
     //
-    public static void drawTriangle(double xShift, double yShift){
-        StdDraw.line(0+xShift,0+yShift, 0.1+xShift, 0.2+yShift);
-        StdDraw.line(0.1+xShift, 0.2+yShift, .2+xShift,0+yShift);
-        StdDraw.line(.2+xShift,0+yShift, 0+xShift, 0+yShift);
+    public static void drawTriangle(double xShift, double yShift, double scale){
+        StdDraw.line(0*scale+xShift,0*scale+yShift, 0.1*scale+xShift, 0.2*scale+yShift);
+        StdDraw.line(0.1*scale+xShift, 0.2*scale+yShift, .2*scale+xShift,0*scale+yShift);
+        StdDraw.line(.2*scale+xShift,0*scale+yShift, 0*scale+xShift, 0*scale+yShift);
     }
 
 }
