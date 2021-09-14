8967daf9aec9b7eeb35a40c70b426163df438c96
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-09-14 09:07:01 -0500

    Create s07_MathRandom.java

---
+++ src/u02_UsingObjects/s07_MathRandom.java
@@ -0,0 +1,31 @@
+package u02_UsingObjects;
+
+public class s07_MathRandom {
+
+    public static void main(String[] args){
+        StdDraw.setScale(0, 100);
+        StdDraw.enableDoubleBuffering(); //Calling this method stops things from being drawn immediately after a draw method is called. This allows you to call many different draw methods without anything being drawn on the screen; when you call StdDraw.show(), everything will be drawn at once.
+
+        int ranNum = (int)(Math.random()*14); //.8796870*10 => 8.796870 => 8
+
+
+        double xPos = 50;
+        double xVel = 10;
+
+        double timeElapsed = 0.017; //0.017 seconds-- this is how long each frame of our animation appears.
+        while(true){
+
+            //Bunny example
+            //              =         50ft    + 1ft/hour * 9hours
+            // What we are trying to do:
+            //new position  = former position + vel*timeElapsed
+            xPos = xPos + xVel*timeElapsed;
+            StdDraw.rectangle(xPos,50,10, 7);
+
+
+            StdDraw.show(); //Because we have called StdDraw.enableDoubleBuffering(), everything that you draw up until this point will be loaded into java's memory but not actually drawn. Calling StdDraw.draw() then draws everything at once that is loaded into java's memory.
+            StdDraw.pause((int)(timeElapsed*1000)); //You must pass to the pause method the number of milliseconds to pause for; so we multiply by 1000 because our timeElapsed variable is in seconds, not milliseconds.
+            //StdDraw.clear(); //This clears everything drawn on the screen. You must redraw the image you wish to display for each frame of an animation
+        }
+    }
+}
