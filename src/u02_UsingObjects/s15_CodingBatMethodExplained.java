fece05a5f8b8760d9d01e0c87d173546b3ee227b
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-09-29 08:44:14 -0500

    Update s15_CodingBatMethodExplained.java

--- src/u02_UsingObjects/s15_CodingBatMethodExplained.java
+++ src/u02_UsingObjects/s15_CodingBatMethodExplained.java
@@ -4,13 +4,10 @@ public class s15_CodingBatMethodExplained {
     public static void main(String args[]){
         String word1 = helloName("Bob");
         System.out.println(word1);
-
         String word2 = helloName("Alice");
         System.out.println(word2);
-
         String word3 = helloName("Hello");
         System.out.println(word3);
-
         String word4 = helloName("ice cream");
         System.out.println(word4);
 
