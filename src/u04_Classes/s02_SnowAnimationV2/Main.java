00d76c39c47aab74808b462e8aaec03ede408763
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-11-09 09:13:22 -0600

    Snowflakes Class

---
+++ src/u04_Classes/s02_SnowAnimationV2/Main.java
@@ -0,0 +1,36 @@
+package u04_Classes.s02_SnowAnimationV2;
+
+public class Main {
+
+    public static void main(String[] args){
+        StdDraw.setScale(0, 100);
+        StdDraw.enableDoubleBuffering(); //Calling this method stops things from being drawn immediately after a draw method is called. This allows you to call many different draw methods without anything being drawn on the screen; when you call StdDraw.show(), everything will be drawn at once.
+
+        Snowflake[] snowflakes = new Snowflake[200];
+        for(int i=0; i<snowflakes.length; i++){
+            snowflakes[i] = new Snowflake();
+        }
+
+
+        double timeElapsed = 0.017; //0.017 seconds-- this is how long each frame of our animation appears.
+        while(true){
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.filledRectangle(50, 50, 60, 60);
+
+            for(int i=0; i<snowflakes.length; i++){
+                snowflakes[i].calculate(timeElapsed);
+            }
+
+
+            for(int i=0; i<snowflakes.length; i++){
+                snowflakes[i].draw();
+            }
+
+
+            StdDraw.show(); //Because we have called StdDraw.enableDoubleBuffering(), everything that you draw up until this point will be loaded into java's memory but not actually drawn. Calling StdDraw.draw() then draws everything at once that is loaded into java's memory.
+            StdDraw.pause((int)(timeElapsed*1000)); //You must pass to the pause method the number of milliseconds to pause for; so we multiply by 1000 because our timeElapsed variable is in seconds, not milliseconds.
+            StdDraw.clear(); //This clears everything drawn on the screen. You must redraw the image you wish to display for each frame of an animation
+        }
+    }
+
+}
---
+++ src/u04_Classes/s02_SnowAnimationV2/Snowflake.java
@@ -0,0 +1,24 @@
+package u04_Classes.s02_SnowAnimationV2;
+
+public class Snowflake {
+    private double xPos;
+    private double yPos;
+    private double yVel;
+
+    public Snowflake(){
+        xPos = Math.random()*100;
+        yPos = Math.random()*100+100;
+        yVel = 30;
+    }
+
+    public void calculate(double timeElapsed){
+        yPos = yPos - yVel*timeElapsed;
+        System.out.println(yPos);
+    }
+
+    public void draw(){
+        StdDraw.setPenColor(StdDraw.WHITE);
+        StdDraw.filledCircle(xPos, yPos, .5);
+    }
+
+}
