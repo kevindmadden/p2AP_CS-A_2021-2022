d465a2b5f8a38084856e94ef930bf458a1719fd7
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-01-13 11:55:12 -0600

    Beginning work to get clicking working

--- src/u05_Connect4/Board.java
+++ src/u05_Connect4/Board.java
@@ -5,7 +5,8 @@ public class Board {
     Token[][] board;
 
     public Board(){
-        board = new Token[6][7];
+        board = new Token[Main.NUM_OF_ROWS][Main.NUM_OF_COLS];
+        //board = new Token[10][40]; //change board dimensions for fun
         for(int row=0; row<board.length; row++){
             for(int col=0; col<board[0].length; col++){
                 System.out.print(board[row][col]);
@@ -19,14 +20,35 @@ public class Board {
         -Each col should be 10 as the width
         -The columns will take up space from 0 to 70 (x-coordinates)
     6 rows
-        -Each row shoud be 10 as the width
-
-
-
+        -Each row should be 10 as the width
      */
 
     public void draw(){
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.filledRectangle(35, 30,35,30);
+        for(int row=0; row<board.length; row++){
+            for(int col=0; col<board[0].length; col++){
+                int x = col*10;
+                int y = row*10;
+                StdDraw.setPenColor(StdDraw.WHITE);
+                StdDraw.filledCircle(x+5, y+5, 4.5);
+            }
+        }
+    }
 
+    //This draw method draws the board accurately regardless of the board's dimensions
+    public void fancyDraw(){
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.filledRectangle(35, 30,35,30);
+        for(int row=0; row<board.length; row++){
+            for(int col=0; col<board[0].length; col++){
+                double x = col*(70.0/board[0].length)+0.5*(70.0/board[0].length); //int x = col*10;
+                double y = row*(60.0/board.length)+0.5*(60.0/board.length); //int y = row*10;
+                StdDraw.setPenColor(StdDraw.WHITE);
+                //StdDraw.filledCircle(x,y, 0.5*(70.0/board[0].length)*.8); //StdDraw.filledCircle(x+5, y+5, 4.5);
+                StdDraw.filledEllipse(x,y, 0.5*(70.0/board[0].length)*.8, 0.5*(60.0/board.length)*.8 );
+            }
+        }
     }
 
 
--- src/u05_Connect4/Main.java
+++ src/u05_Connect4/Main.java
@@ -1,19 +1,57 @@
 package u05_Connect4;
 
 public class Main {
+    static final int NUM_OF_ROWS = 6;
+    static final int NUM_OF_COLS = 7;
+
+
     public static void main(String[] arg){
         Board board = new Board();
 
 
 
-        StdDraw.setScale(0, 100);
+        StdDraw.setScale(0, 70);
         StdDraw.enableDoubleBuffering(); //Calling this method stops things from being drawn immediately after a draw method is called. This allows you to call many different draw methods without anything being drawn on the screen; when you call StdDraw.show(), everything will be drawn at once.
+
+        boolean prevMouseClickedValue = false;
+        boolean didClickOccur = false;
 
         double timeElapsed = 0.017; //0.017 seconds-- this is how long each frame of our animation appears.
         while(true){
+            //Mouse-clicking logic - Keep at top
+            if(!StdDraw.isMousePressed() && prevMouseClickedValue){
+                didClickOccur = true;
+                System.out.println(StdDraw.mouseX()+ ", "+StdDraw.mouseY());
+            }
+            prevMouseClickedValue = StdDraw.isMousePressed();
+
+            if(didClickOccur){
+                int colNumToDropIn;
+                for(int colNum = 0; colNum < Main.NUM_OF_COLS; colNum++ ){
+                    if( StdDraw.mouseX() < (colNum+1)*10){
+                        colNumToDropIn = colNum;
+                        break;
+                    }
+                }
+
+
+                if(StdDraw.mouseX() < 10){
+                    colNumToDropIn = 0;
+                }else if(StdDraw.mouseX() < 20){
+                    colNumToDropIn = 1;
+                }else if(StdDraw.mouseX() < 30){
+                    colNumToDropIn = 2;
+                }
+
+            }
+
+
+
+
+
+
 
             board.draw();
-
 
 
             StdDraw.show(); //Because we have called StdDraw.enableDoubleBuffering(), everything that you draw up until this point will be loaded into java's memory but not actually drawn. Calling StdDraw.draw() then draws everything at once that is loaded into java's memory.
